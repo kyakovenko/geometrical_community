e19838d1ccd7da8cf53d8aaa1cfd71dae93005a6
Kirill S. Yakovenko <kirill.yakovenko@gmail.com>
2013-04-29 01:51:19 +0700

    grey and white blocks change places

--- geometry/cms_plugin_processors.py
+++ geometry/cms_plugin_processors.py
@@ -6,8 +6,7 @@ from django.template.loader import render_to_string
 
 
 def wrap_content(instance, placeholder, rendered_content, original_context):
-    if placeholder.slot != 'content' or instance.plugin_type == 'TextPlugin' \
-            or (instance._render_meta.text_enabled and instance.parent):
-        return rendered_content
+    if original_context.get('plugin_index') % 2 == 0:
+        return render_to_string('cms/white-wrapper.html', {"content": rendered_content})
     else:
         return render_to_string('cms/gray-wrapper.html', {"content": rendered_content})
